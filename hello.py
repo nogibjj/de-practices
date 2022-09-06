37073cc25b6e97bd9c67fb0dc76540636c376902
celisa <elisa96chen@gmail.com>
2022-09-06 01:49:09 +0000

    modified hello.py using copilot

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
-#write a hello function that prints "Hello, World!"
+#write a hello function that prints "Hello, World!" and returns "Hello, World!"
 def hello():
+    """Prints "Hello, World!" and returns "Hello, World!" """
     print("Hello, World!")
+    return "Hello, World!"
 
 #invoke hello function
 hello()
-

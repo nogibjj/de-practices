84e6be5366d5527eaa4119b7298400dbac60f7ad
celisa <elisa96chen@gmail.com>
2022-09-06 03:20:56 +0000

    update repo with summarizer tool using open AI

--- build_summarizer.py
+++ build_summarizer.py
@@ -11,16 +11,26 @@ import click
 # write a function that takes a text string and returns a summary of the text
 def summarize(text):
     """Summarize a text string and return a summary of the text"""
-    openai.api_key = os.getenv("OPEN_AI_SECRET_KEY")
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
     response = openai.Completion.create(
-        engine="davinci",
+        model="text-davinci-002",
         prompt=text,
-        temperature=0.9,
-        max_tokens=150,
+        temperature=0.7,
+        max_tokens=256,
         top_p=1,
         frequency_penalty=0,
-        presence_penalty=0.6,
-        stop=["\n", " Human:", " AI:"],
+        presence_penalty=0,
     )
-    return response.choices[0].text
+    return response["choices"][0]["text"].strip(" \n")
+
+
+@click.command()
+@click.argument("text")
+def main(text):
+    """Summarize a text string and return a summary of the text"""
+    print(summarize(text))
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    main()
